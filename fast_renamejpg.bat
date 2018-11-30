6e63d2c144589fd37f08c5bab405fb9dbc56c4e4
Teacher Lu <lz9168@163.com>
2018-11-30 19:43:12 +0800

    rename any files
    MS BAT

---
+++ fast_renamejpg.bat
@@ -0,0 +1,40 @@
+:::::::批量修改jpg文件名.bat:::::::
+@echo off
+title 批量修改jpg文件名
+setlocal EnableDelayedExpansion
+:: 启用延迟变量扩充
+ 
+:GetPath
+set zpath=%CD%
+ 
+:GetPrefix
+set prefix=未命名
+set /p prefix=请输入新前缀(禁用字符\/:*?"<>|)：
+for /f "delims=\/:*?<>| tokens=2" %%i in ("z%prefix%z") do goto :GetPrefix
+ 
+:GetExt
+set ext=.jpg
+ 
+:ReadyToRename
+ 
+set /a num=1
+
+for %%i in ("%zpath%\*%ext%") do (
+echo "!num!%%i"
+
+    if !num! LSS 10 (
+        ren "%%i" "%prefix%0!num!%ext%" || echo 文件 %%i 改名失败 && set /a num-=1
+    ) else ( 
+        ren "%%i" "%prefix%!num!%ext%" || echo 文件 %%i 改名失败 && set /a num-=1
+    )
+
+    set /a num+=1
+)
+ 
+if %num%==0 echo %zpath%\ 里未发现任何文件。按任意键退出... & goto :PauseThenQuit
+
+echo 文件改名完成，按任意键退出...
+ 
+:PauseThenQuit
+:pause>nul
+::::::::::::::::::::::::::::::::
---
+++ rename去括号.bat
@@ -0,0 +1,14 @@
+:: 改写“img (1).jpg”这样的文件名称为 “img_1.jpg”
+
+@Echo Off & SetLocal ENABLEDELAYEDEXPANSION
+
+FOR %%a in (*) do (
+  set "name=%%a"
+
+::如果不想要下划线，就直接去掉就可以
+  set "name=!name: (=_!"
+  set "name=!name:)=!"
+  ren "%%a" "!name!"
+)
+
+exit
